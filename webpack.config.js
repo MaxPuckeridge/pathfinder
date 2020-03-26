2ea0154cdfbdf68e030656d1d260e23f359345ac
Max Puckeridge <max.puckeridge@gmail.com>
2020-03-26 15:49:10 +1100

    Typescript convert

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,18 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   mode: 'development',
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
-        options: { presets: ['@babel/env'] },
-      }
+      },
     ],
   },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
+  resolve: { extensions: ['.ts', '.tsx', '.js', '.json'] },
   output: {
     path: path.resolve(__dirname, 'dist/'),
     publicPath: '/dist/',
